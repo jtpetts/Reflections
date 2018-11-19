b0583524b72d411f75f61c19529d7daa60a16464
John Petts <jtpetts@gmail.com>
2018-11-19 09:23:59 -0800

    Fixing Env Variables for backend

--- reflectionsui/src/services/httpService.jsx
+++ reflectionsui/src/services/httpService.jsx
@@ -4,6 +4,8 @@ function setJwt(authToken) {
   axios.defaults.headers.common["x-auth-token"] = authToken;
 }
 
+//axios.defaults.baseURL = process.env.REACT_APP_REFLECTIONS_API;
+
 // Add a response interceptor
 axios.interceptors.response.use(null, error => {
   const expectedError =
