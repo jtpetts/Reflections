58917c19e774cf5b3332ba7dddfed15324c5bdd1
John Petts <jtpetts@gmail.com>
2018-11-18 21:45:02 -0800

    First commit in new location

---
+++ ReflectionsApi/startup/database.js
@@ -0,0 +1,14 @@
+﻿const mongoose = require('mongoose');
+const config = require('config');
+const winston = require('winston');
+
+module.exports = function () {
+
+    console.log('jwt key [', config.jwtPrivateKey, ']');
+    console.log('MongoDBUrl:', config.MongoDBUrl);
+    console.log('NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
+    //set REFLECTIONS_JWTPRIVATEKEY=jwtprivatekeyshouldbeinEnvVariable
+
+    mongoose.connect(config.MongoDBUrl, { useNewUrlParser: true })
+        .then(() => winston.info(`Connected to MongoDB ${config.MongoDBUrl}...`));
+}
---
+++ ReflectionsApi/tests/integration/routes/UsersRoute.texxst.js
@@ -0,0 +1,43 @@
+﻿const request = require('supertest');
+const bcrypt = require('bcryptjs');
+const { UserModel } = require('../../../models/UserModel');
+
+let server;
+
+describe('/api/Auth', () => {
+
+    beforeEach(async () => {
+        server = require('../../../index');
+        await UserModel.remove({});
+    });
+
+    afterEach(() => {
+        server.close();
+    });
+
+    async function insertUser() {
+
+        const salt = await bcrypt.genSalt(10);
+        hashedPassword = await bcrypt.hash('wierdisspelledwrong', salt);
+
+        return await UserModel.collection.insertMany([
+            {
+                name: 'siby',
+                email: 'siby@gmail.com',
+                password: hashedPassword,
+            }
+        ]);
+    }
+
+    it('should ', async () => {
+
+        //request.body.email = 'siby@gmail.com';
+        //request.body.password = 'wierdisspelledwrong';
+
+        //const response = await request(server).post('/api/auth');
+
+        //expect(response.status).toBe(404);
+    });
+
+});
+
