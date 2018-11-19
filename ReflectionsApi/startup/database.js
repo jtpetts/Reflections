66d6ca0624faf89542de46efb03a4adcad92fc79
John Petts <jtpetts@gmail.com>
2018-11-19 07:54:47 -0800

    Correcting the env variables.

--- ReflectionsApi/startup/database.js
+++ ReflectionsApi/startup/database.js
@@ -1,14 +1,12 @@
-﻿const mongoose = require('mongoose');
-const config = require('config');
-const winston = require('winston');
+﻿const mongoose = require("mongoose");
+const config = require("config");
+const winston = require("winston");
 
-module.exports = function () {
-
-    console.log('jwt key [', config.jwtPrivateKey, ']');
-    console.log('MongoDBUrl:', config.MongoDBUrl);
-    console.log('NODE_ENV: ' + config.util.getEnv('NODE_ENV'));
-    //set REFLECTIONS_JWTPRIVATEKEY=jwtprivatekeyshouldbeinEnvVariable
-
-    mongoose.connect(config.MongoDBUrl, { useNewUrlParser: true })
-        .then(() => winston.info(`Connected to MongoDB ${config.MongoDBUrl}...`));
-}
+module.exports = function() {
+  mongoose
+    .connect(
+      config.MongoDBUrl,
+      { useNewUrlParser: true }
+    )
+    .then(() => winston.info(`Connected to MongoDB ${config.MongoDBUrl}...`));
+};
